452f1bd87f8968cff24a2bb9a3b4c34886d76367
Marco Vignati <marcovignatidev@gmail.com>
2024-03-04 16:59:38 -0400

    first commit

---
+++ src/main/java/br/com/marcovignati/rinhabackendq1/RinhaBackendQ1Application.java
@@ -0,0 +1,13 @@
+package br.com.marcovignati.rinhabackendq1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackendQ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackendQ1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcovignati/rinhabackendq1/RinhaBackendQ1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcovignati.rinhabackendq1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackendQ1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
